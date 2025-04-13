15b5a8a6a2a07baef27aec1d37a00510cc5ff5e8
Aniruddha Surse <aniruddhasurse@Aniruddhas-MacBook-Air.local>
2025-04-14 04:33:56 +0530

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
     skipInline: false,
     drafts: true,
   },
-  site: "https://yourwebsite.com",
+  site: "https://avsblogs.netlify.app",
   integrations: [
     sitemap({
       changefreq: "weekly",
